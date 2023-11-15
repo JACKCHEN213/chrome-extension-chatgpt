14f30adb06d1e519c637911d88e07ce9897c724d
jackc <1366294101@qq.com>
2023-11-15 22:24:53 +0800

    :recycle: refactor: 完成登录逻辑

--- js/login.js
+++ js/login.js
@@ -1,6 +1,15 @@
+function initOldData() {
+    let loginInfo = window.localStorage.getItem('login_info');
+    if (!loginInfo) {
+        return;
+    }
+    loginInfo = JSON.parse(atob(loginInfo));
+    $('#username').val(loginInfo.username);
+    $('#password').val(loginInfo.password);
+    $('#remember').attr('checked', 'checked');
+}
 
-
-function init() {
+function registerListener() {
     $('#password-eye').on('click', function () {
         if ($(this).hasClass('bi-eye-slash')) {  // 显示密码
             $(this).removeClass('bi-eye-slash').addClass('bi-eye');
@@ -12,28 +21,47 @@ function init() {
     });
     $('#login-btn').on('click', function () {
         let username = $('#username').val().trim();
+        $('#username-feedback').hide();
+        if (!username) {
+            $('#username-feedback').html('用户名不能为空').show();
+            return;
+        }
         let password = $('#password').val().trim();
+        $('#password-feedback').hide();
+        if (!password) {
+            $('#password-feedback').html('密码不能为空').show();
+            return;
+        }
         let remember = $('#remember').get(0).checked;
-        // $('#username-feedback').show();
-        // $('#password-feedback').show();
         $.ajax({
             type: 'POST',
             url: 'http://192.168.24.20:3001/api/User/login',
             contentType: 'application/json;charset=utf8',
             data: JSON.stringify({
-                username,
-                password: btoa(md5(password))
+                username, password: btoa(md5(password))
             }),
             success: (data) => {
                 if (data.success) {
-                    console.log(data)
-                    messageEx('登录成功', 'success', 500);
+                    let accountInfo = btoa(JSON.stringify(data.data));
+                    window.localStorage.setItem('account_info', accountInfo);
+                    if (remember) {
+                        window.localStorage.setItem('login_info', btoa(JSON.stringify({
+                            username, password
+                        })));
+                    }
+                    toastr.success('登录成功');
+                    window.location = 'popup.html';
                 } else {
-                    messageEx(data.message, 'error');
+                    $('#username-feedback').html(data.message).show();
                 }
             }
         });
     });
+}
+
+function init() {
+    initOldData();
+    registerListener();
 }
 
 $(document).ready(function () {
--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,13 @@
 function sendMessage() {
-  let message = $('#message-input').val();
-  if (!message) {
-    return;
-  }
-  message.replace(/&quot;/g, '"')
-    .replace(/&#96;/g, '`')
-    .replace(/&#36;/g, '$')
-    .replace(/&lt;/g, '<');
-  let element = $(`
+    let message = $('#message-input').val();
+    if (!message) {
+        return;
+    }
+    message.replace(/&quot;/g, '"')
+        .replace(/&#96;/g, '`')
+        .replace(/&#36;/g, '$')
+        .replace(/&lt;/g, '<');
+    let element = $(`
     <li class="chat-item">
       <div class="role-man">
         <img class="avatar-24" src="images/icon.png" alt="avatar" />
@@ -18,42 +18,82 @@ function sendMessage() {
         </div>
       </div>
     </li>`);
-  $('#chat-content ul').append(element);
-  $('#message-input').val('');
-  hljs.highlightAll();
-  hljs.initCopyButtonOnLoad();
+    $('#chat-content ul').append(element);
+    $('#message-input').val('');
+    hljs.highlightAll();
+    hljs.initCopyButtonOnLoad();
+}
+
+function loginOut() {
+    window.localStorage.removeItem('account_info');
+    window.location = 'login.html';
+}
+
+function initAuth() {
+    let accountInfo = window.localStorage.getItem('account_info');
+    if (!accountInfo) {
+        loginOut();
+        return;
+    }
+    accountInfo = JSON.parse(atob(accountInfo));
+    $.ajax({
+        type: 'POST',
+        url: 'http://192.168.24.20:3001/api/User/verify',
+        contentType: 'application/json;charset=utf8',
+        async: false,
+        data: JSON.stringify({ token: accountInfo.token }),
+        success: (data) => {
+            if (data.success) {
+                //
+            } else {
+                loginOut();
+            }
+        },
+        error: (error) => {
+            loginOut();
+        }
+    })
+}
+
+function initOldData() {
+    marked.setOptions({
+        highlight: function (code, language) {
+            const validLanguage = hljs.getLanguage(language) ? language : 'javascript';
+            return hljs.highlight(code, {language: validLanguage}).value;
+        }
+    });
+    let messageList = $('#chat-content ul li div.chat-display-message');
+    for (const messageElement of messageList) {
+        let message = $(messageElement).html()
+            .replace(/&quot;/g, '"')
+            .replace(/&#96;/g, '`')
+            .replace(/&#36;/g, '$')
+            .replace(/&lt;/g, '<');
+        $(messageElement).html(marked.parse(message));
+    }
+    hljs.highlightAll();
+    hljs.initCopyButtonOnLoad();
+}
+
+function registerListener() {
+    $('#send-button').on('click', sendMessage);
+    $(document).keydown(function (event) {
+        if (event.ctrlKey && event.key === 'Enter') {
+            sendMessage();
+        }
+    });
+    $('#login-out').on('click', function () {
+        window.localStorage.removeItem('account_info');
+        window.location = 'login.html';
+    });
 }
 
 function init() {
-  $('#send-button').on('click', sendMessage);
-  marked.setOptions({
-    highlight: function (code, language) {
-      const validLanguage = hljs.getLanguage(language) ? language : 'javascript';
-      return hljs.highlight(code, { language: validLanguage }).value;
-    }
-  });
-  let messageList = $('#chat-content ul li div.chat-display-message');
-  for (const messageElement of messageList) {
-    let message = $(messageElement).html()
-      .replace(/&quot;/g, '"')
-      .replace(/&#96;/g, '`')
-      .replace(/&#36;/g, '$')
-      .replace(/&lt;/g, '<');
-    $(messageElement).html(marked.parse(message));
-  }
-  hljs.highlightAll();
-  hljs.initCopyButtonOnLoad();
-
-  $(document).keydown(function (event) {
-    if (event.ctrlKey && event.key === 'Enter') {
-      sendMessage();
-    }
-  });
-  $('#login-out').on('click', function () {
-    window.location = 'login.html';
-  });
+    initAuth();
+    initOldData();
+    registerListener();
 }
 
 $(document).ready(function () {
-  init();
+    init();
 });
